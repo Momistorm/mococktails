cc8c6b4a01eed20daae32b6082bd107533d65f93
Momistorm <moritz.weissenhofer@proton.me>
2026-06-15 17:18:36 +0200

    seriously didnt work because of a missing semicolon

--- src/svelte.config.js
+++ src/svelte.config.js
@@ -4,16 +4,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {any} */
 const config = {
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter({
-    pages: 'build',
-    assets: 'build',
-    fallback: '404.html', // Das hier ist der Schlüssel!
-    precompress: false,
-    strict: true
-})
-		// NEU: Das verhindert, dass der Build bei dynamischen API-Routen abbricht
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		}),
 		prerender: {
 			handleUnseenRoutes: 'ignore'
 		}
